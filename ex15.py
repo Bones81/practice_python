be1398af7bd5878a85a8d39cc7628baea82b8026
Nathan Freeman <nathanlfreeman@gmail.com>
2023-05-19 14:31:15 -0400

    15 add user input

--- ex15.py
+++ ex15.py
@@ -1,15 +1,8 @@
 def reverseString(lString):
     wordList = lString.split()
-    print(wordList)
-
     revList = wordList[::-1]
-    print(revList)
-
     result = " ".join(revList)
-    print(result)
-
     return result
 
-longString = "Hello world, my name is Nathan, and I am a software engineer."
-
-reverseString(longString)
+longString = input("Enter a long string of words to reverse: ")
+print(reverseString(longString))
