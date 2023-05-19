54ea971533c77420d2ac254c2ed44a579d055981
Nathan Freeman <nathanlfreeman@gmail.com>
2023-05-19 14:22:48 -0400

    14 done

---
+++ ex14.py
@@ -0,0 +1,14 @@
+def convertList(list):
+    result = []
+    for item in list:
+        if (item not in result):
+            result.append(item)
+    return result
+
+list1 = ['Nate', 'Sujan', 'Jeff', 'Nate', 'Sujan']
+
+def setFromList(list):
+    return set(list)
+
+print(convertList(list1))
+print(setFromList(list1))
